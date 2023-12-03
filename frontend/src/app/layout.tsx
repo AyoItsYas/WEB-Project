efce9975d36a180d28c60ffca4e758c1b7d21317
RehanDD <rehandayarathne@gmail.com>
2023-12-03 08:34:06 -0500

    FooterItems

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         </NavBar>
         {children}
         <Footer>
-
+          
         </Footer>
       </body>
     </html>
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,23 @@
 import styles from './Footer.module.scss';
+import React from 'react';
 
-function Footer(){
-  return (
-    <footer className={styles.Footer}>
-      <p>hellowtesting</p>
-    </footer>
-  )
+function FooterModule(){
+    return (
+        <footer className={styles.Footer}>
+          <div>
+            <h4>About Us</h4>
+            <p>We are Kpop Stans</p>
+          </div>
+          <div>
+            <h4>Contact Us</h4>
+            <p>Email: contact@example.com</p>
+            <p>Phone: +1 (123) 456-7890</p>
+          </div>
+          <div>
+            <h1>Sue us here</h1>
+          </div>
+        </footer>
+    )
 }
 
-export default Footer;
+export default FooterModule;
