d29aeb3f666c7a48667ab3439d163993a5281265
Nibbles0 <dulsaraseekkuge@gmail.com>
2023-12-03 17:35:22 +0530

    Changes to Signup

--- backend/src/auth/signup.php
+++ backend/src/auth/signup.php
@@ -29,5 +29,42 @@ function handler(): array
 
   // processing the data through our endpoint logic
   $DB_CONN = connect("users"); // connecting to the users table in the database
+  
+  // Check if user already exists
+  $SQL = "SELECT * FROM users WHERE email = '$EMAIL'"; // the query to be executed
+  $RESULT = mysqli_query($DB_CONN, $SQL); // executing the query we get an array of rows as a result
+
+  if (mysqli_num_rows($RESULT) > 0) {
+    $STST = 400;
+    $DATA = [
+      "status" => $STST,
+      "error" => "User already exists"
+    ];
+
+    return [$STST, $DATA];
+  }
+
+  // If user doesn't exist, create a new user
+  $SQL = "INSERT INTO users (email, password) VALUES ('$EMAIL', '$PASSWORD')";
+  $RESULT = mysqli_query($DB_CONN, $SQL);
+
+  if ($RESULT) {
+    $STST = 200;
+    $DATA = [
+      "status" => $STST,
+      "message" => "User created successfully"
+    ];
+  } else {
+    $STST = 400;
+    $DATA = [
+      "status" => $STST,
+      "error" => "Failed to create user"
+    ];
+  }
+
+  return [$STST, $DATA];
+}
+
+respond(...handler());
 
 ?>
