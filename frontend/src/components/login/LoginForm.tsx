b08fdace39f461a157539f1fb9f6259385a0f8f1
AyoItsYas <yasiru.dharmathilaka@gmail.com>
2023-12-05 11:51:36 +0530

    Refactor login and signup forms

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import style from './Common.module.scss';
+import styles from './Common.module.scss';
 
 
 export default function LoginForm() {
   return (
-    <div className={`LoginForm ${style.login}`}>
-    <h2 className={style.h4}>Login</h2>
-    <form className={style.form}>
+    <div className={styles.LoginForm}>
+    <h2>Login</h2>
+
+    <form>
       <table>
         <tbody>
           <tr>
             <td><label htmlFor="username">Username:</label></td>
-            <td><input className={`${style.text_input} ${style.text_area}`} type="text" id="username" name="username" placeholder="Enter your username" /></td>
+            <td><input  className={`${styles.textInput} ${styles.textArea}`} type="text" id="username" name="username" placeholder="Enter your username" /></td>
           </tr>
+
           <tr>
             <td><label htmlFor="password">Password:</label></td>
-            <td><input className={`${style.text_input} ${style.text_area}`} type="password" id="password" name="password" placeholder="Enter your password" /></td>
+            <td><input className={`${styles.textInput} ${styles.textArea}`} type="password" id="password" name="password" placeholder="Enter your password" /></td>
           </tr>
+
           <tr>
-            <td colSpan={2} align="center"><button className={style.btn} type="submit">Login</button></td>
+            <td colSpan={2} align="center"><button type="submit">Login</button></td>
           </tr>
         </tbody>
       </table>
--- frontend/src/components/login/SignupForm.tsx
+++ frontend/src/components/login/SignupForm.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
-import style from './Common.module.scss';
+import styles from './Common.module.scss';
 
 export default function SignupForm() {
   return (
-  <div className={`SignupForm ${style.login}`}>
-  <h2 className={style.h4}>Sign Up</h2>
-  <form className={style.form}>
-    <table>
-      <tbody>
-        <tr>
-          <td><label htmlFor="fullName">Full Name:</label></td>
-          <td><input className={`${style.text_input} ${style.text_area}`} type="text" id="fullName" name="fullName" placeholder="Enter your full name" /></td>
-        </tr>
-        <tr>
-          <td><label htmlFor="email">Email:</label></td>
-          <td><input className={`${style.text_input} ${style.text_area}`} type="email" id="email" name="email" placeholder="Enter your email" /></td>
-        </tr>
-        <tr>
-          <td><label htmlFor="newPassword">New Password:</label></td>
-          <td><input className={`${style.text_input} ${style.text_area}`} type="password" id="newPassword" name="newPassword" placeholder="Enter a new password" /></td>
-        </tr>
-        <tr>
-          <td colSpan={2} align="center"><button className={style.btn} type="submit">Sign Up</button></td>
-        </tr>
-      </tbody>
-    </table>
-  </form>
-</div>
-)
+    <div className={styles.LoginForm}>
+    <h2>Sign Up</h2>
+    <form>
+      <table>
+        <tbody>
+          <tr>
+            <td><label htmlFor="fullName">Full Name:</label></td>
+            <td><input className={`${styles.textInput} ${styles.textArea}`} type="text" id="fullName" name="fullName" placeholder="Enter your full name" /></td>
+          </tr>
+
+          <tr>
+            <td><label htmlFor="email">Email:</label></td>
+            <td><input className={`${styles.textInput} ${styles.textArea}`} type="email" id="email" name="email" placeholder="Enter your email" /></td>
+          </tr>
+
+          <tr>
+            <td><label htmlFor="newPassword">New Password:</label></td>
+            <td><input className={`${styles.textInput} ${styles.textArea}`} type="password" id="newPassword" name="newPassword" placeholder="Enter a new password" /></td>
+          </tr>
+
+          <tr>
+            <td colSpan={2} align="center"><button type="submit">Sign Up</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  </div>
+  )
 };
