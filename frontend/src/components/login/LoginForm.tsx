5683871e2cf2d3e8c0659154ec33ba1e53e73d1f
RehanDD <rehandayarathne@gmail.com>
2023-12-05 00:51:57 -0500

    Adding css to login and signup page part 4

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -3,17 +3,25 @@ import style from './Common.module.scss';
 
 
 export default () => (
-  <div className="LoginForm">
-    <h2>Login</h2>
-    <form>
-      <label htmlFor="username">Username:</label>
-      <input type="text" id="username" name="username" placeholder="Enter your username" />
-
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" placeholder="Enter your password" />
-
-      <button type="submit">Login</button>
-    </form>
-  </div>
+  <div className={`LoginForm ${style.login}`}>
+  <h2 className={style.h4}>Login</h2>
+  <form className={style.form}>
+    <table>
+      <tbody>
+        <tr>
+          <td><label htmlFor="username">Username:</label></td>
+          <td><input className={`${style.text_input} ${style.text_area}`} type="text" id="username" name="username" placeholder="Enter your username" /></td>
+        </tr>
+        <tr>
+          <td><label htmlFor="password">Password:</label></td>
+          <td><input className={`${style.text_input} ${style.text_area}`} type="password" id="password" name="password" placeholder="Enter your password" /></td>
+        </tr>
+        <tr>
+          <td colSpan={2} align="center"><button className={style.btn} type="submit">Login</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </form>
+</div>
 );
 
--- frontend/src/components/login/SignupForm.tsx
+++ frontend/src/components/login/SignupForm.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import style from './Common.module.scss';
 
 export default () => (
-  <div className="SignupForm">
-    <h2>Sign Up</h2>
-    <form>
-      <label htmlFor="fullName">Full Name:</label>
-      <input type="text" id="fullName" name="fullName" placeholder="Enter your full name" />
-
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" placeholder="Enter your email" />
-
-      <label htmlFor="newPassword">New Password:</label>
-      <input type="password" id="newPassword" name="newPassword" placeholder="Enter a new password" />
-
-      <button type="submit">Sign Up</button>
-    </form>
-  </div>
+  <div className={`SignupForm ${style.login}`}>
+  <h2 className={style.h4}>Sign Up</h2>
+  <form className={style.form}>
+    <table>
+      <tbody>
+        <tr>
+          <td><label htmlFor="fullName">Full Name:</label></td>
+          <td><input className={`${style.text_input} ${style.text_area}`} type="text" id="fullName" name="fullName" placeholder="Enter your full name" /></td>
+        </tr>
+        <tr>
+          <td><label htmlFor="email">Email:</label></td>
+          <td><input className={`${style.text_input} ${style.text_area}`} type="email" id="email" name="email" placeholder="Enter your email" /></td>
+        </tr>
+        <tr>
+          <td><label htmlFor="newPassword">New Password:</label></td>
+          <td><input className={`${style.text_input} ${style.text_area}`} type="password" id="newPassword" name="newPassword" placeholder="Enter a new password" /></td>
+        </tr>
+        <tr>
+          <td colSpan={2} align="center"><button className={style.btn} type="submit">Sign Up</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </form>
+</div>
 );
