0d56b5dbe6cb9109295099e820c0d90a0fa0752e
minababo <minada965@gmail.com>
2023-12-12 21:14:06 +0530

    Added sql queries

--- backend/init.sql
+++ backend/init.sql
@@ -26,6 +26,68 @@ CREATE TABLE IF NOT EXISTS products (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS products (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255),
+  price DECIMAL(10, 2),
+  description TEXT,
+  image VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+  id INT NOT NULL AUTO_INCREMENT,
+  rating INT,
+  review TEXT,
+  email VARCHAR(255),
+  product_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255),
+  price DECIMAL(10, 2),
+  description TEXT,
+  image VARCHAR(255),
+  popularity INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255),
+  price DECIMAL(10, 2),
+  description TEXT,
+  image VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
 ALTER USER 'user'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE ON @dbName.* TO 'user'@'%';
 FLUSH PRIVILEGES;
