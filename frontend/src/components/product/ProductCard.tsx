d8f4cfb233469941dc1eb02d1a32283a7140db7e
AyoItsYas <yasiru.dharmathilaka@gmail.com>
2023-12-14 23:11:50 +0530

    Refactor ProductCard component to improve readability

--- frontend/src/components/product/ProductCard.tsx
+++ frontend/src/components/product/ProductCard.tsx
@@ -26,7 +26,8 @@ export default function ProductCard({
         </div>
 
         <div className={styles.ProductInfo}>
-          <h3>{product.name}</h3>g<label>{product.price} LKR</label>
+          <h3>{product.name}</h3>
+          <label>{product.price} LKR</label>
         </div>
       </Link>
     </div>
